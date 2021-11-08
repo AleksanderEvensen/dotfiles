a8892887e5403389c320733fbb3ca2d3d1319b07
Aleksander Evensen <ahse03@gmail.com>
2021-11-08 20:05:42 +0100

    Done with the download script

--- download.ps1
+++ download.ps1
@@ -1,42 +1,48 @@
-function PromptUser($title, $desc, $choices, $default = -1) {
-    $options = ("hello world", "test");
-    # for( $i=0; $i -lt $choices.length; $i++) {
-    #     write-host $choices[$i]
-    #     $options.Add((New-Object System.Management.Automation.Host.ChoiceDescription $choices[$i] ) ) 
-    # }
-    write-host $title
-    write-host $desc
-    write-host $default
-    Write-Output $choices -NoEnumerate | Measure-Object
-    Write-Output $options -NoEnumerate | Measure-Object
-    # Write-Output $options
-    return 0;
-    return $host.ui.PromptForChoice($title, $desc, $options, 1)
+function PromptUser {
+
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$title, 
+        [Parameter(Mandatory=$true, Position=1)]
+        [string]$desc, 
+        [Parameter(Mandatory=$true, Position=2)]
+        [string[][]]$choices, 
+        [Parameter(Mandatory=$false, Position=3)]
+        [int]$default = -1
+    )
+
+    $options = @();
+    foreach ($choice in $choices) {
+        write-host $choice;
+        $options += (New-Object System.Management.Automation.Host.ChoiceDescription $choice) 
+    }
+
+    Write-Host $options
+    return $host.ui.PromptForChoice($title, $desc, $options, $default)
 }
 
 
-$result = PromptUser("Title", "Description", @(
-    ("&Yes", "Idk"),
-    ("&No", "Idk 2")
-), 1)
 
+$result = PromptUser -title "Run the Setup Script" -desc "Should we run the setup.ps1 script after the insallation. This script will install everything needed"  -choices @(
+    @("&yes", "Yes"),
+    @("&no", "No")
+) -default 1
 
-switch ($result) {
-  0 {
-    Write-Host "Yes"
-  }
+write-host "Installing git..."
+# Download git cli tool
+winget install Git.Git
+write-host "Git installed"
+write-host "Cloning repo"
+# Cloning the repo
+pushd $HOME
+git clone https://github.com/AleksanderEvensen/dotfiles.git
+popd
 
-  1 {
-    Write-Host "No"
-  }
+cd $HOME/dotfiles
+
+if ($result -eq 0) {
+    Invoke-Expression -Command "./setup.ps1"
+} else {
+    write-host "Skipping script execution. start the setup.ps1 script manually to complete the setup"
 }
-# write-host "Installing git..."
-# # Download git cli tool
-# winget install Git.Git
-# write-host "Git installed"
-# write-host "Cloning repo"
-# pushd $HOME
-# git clone https://github.com/AleksanderEvensen/dotfiles.git
-# popd
-# cd $HOME/dotfiles
 
